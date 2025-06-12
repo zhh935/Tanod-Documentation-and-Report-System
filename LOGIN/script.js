b84252dd1835881ac302a5f75b02cccc1ab3349e
desilily <desireedelmonte143316@gmail.com>
2025-06-12 20:13:24 +0800

    git add git commit -m "updated code" git push

--- LOGIN/script.js
+++
@@ -1,4 +0,0 @@
-function showForm(formId) {
-   document.querySelectorAll("btn employee active").forEach(form => form.classList.remove("active"));
-   document.getElementById(formId).classList.add("active");
-}
